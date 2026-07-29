ba2d423aeaf9a4acca4216eaa6f8b541eaba76f4
konstantin <konst.gromov@gmail.com>
2026-07-30 01:41:26 +0300

    Show case for record usage

--- src/main/java/org/kgromov/mappers/CountryLanguageMapper.java
+++ src/main/java/org/kgromov/mappers/CountryLanguageMapper.java
@@ -3,26 +3,27 @@ package org.kgromov.mappers;
 import org.apache.ibatis.annotations.*;
 import org.kgromov.model.CountryLanguage;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Mapper
 public interface CountryLanguageMapper {
 
     @Select("SELECT * FROM countrylanguage")
-    @Results({
-            @Result(id = true, property = "code", column = "CountryCode"),
-            @Result(id = true, property = "language", column = "Language"),
-            @Result(property = "official", column = "IsOfficial"),
-            @Result(property = "usage", column = "Percentage")
+    @ConstructorArgs({
+            @Arg(id = true, name = "code", javaType = String.class, column = "CountryCode"),
+            @Arg(id = true, name = "language", javaType = String.class, column = "Language"),
+            @Arg(name = "official", javaType = Boolean.class, column = "IsOfficial"),
+            @Arg(name = "usage", javaType = BigDecimal.class, column = "Percentage")
     })
     List<CountryLanguage> findAll();
 
     @Select("SELECT * FROM countrylanguage WHERE countrycode = #{CountryCode} AND language = #{Language}")
-    @Results({
-            @Result(id = true, property = "code", column = "CountryCode"),
-            @Result(id = true, property = "language", column = "Language"),
-            @Result(property = "official", column = "IsOfficial"),
-            @Result(property = "usage", column = "Percentage")
+    @ConstructorArgs({
+            @Arg(id = true, name = "code", javaType = String.class, column = "CountryCode"),
+            @Arg(id = true, name = "language", javaType = String.class, column = "Language"),
+            @Arg(name = "official", javaType = Boolean.class, column = "IsOfficial"),
+            @Arg(name = "usage", javaType = BigDecimal.class, column = "Percentage")
     })
     CountryLanguage findById(@Param("CountryCode") String code,
                              @Param("Language") String language);
--- src/main/java/org/kgromov/model/CountryLanguage.java
+++ src/main/java/org/kgromov/model/CountryLanguage.java
@@ -2,62 +2,10 @@ package org.kgromov.model;
 
 import java.math.BigDecimal;
 
-public class CountryLanguage {
-    private String code;
-    private String language;
-    private Boolean official;
-    private BigDecimal usage;
-
-
-    public CountryLanguage() {
-    }
-
-    public CountryLanguage(String code, Boolean official, String language, BigDecimal usage) {
-        this.code = code;
-        this.official = official;
-        this.language = language;
-        this.usage = usage;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public Boolean getOfficial() {
-        return official;
-    }
-
-    public void setOfficial(Boolean official) {
-        this.official = official;
-    }
-
-    public String getLanguage() {
-        return language;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-
-    public BigDecimal getUsage() {
-        return usage;
-    }
-
-    public void setUsage(BigDecimal usage) {
-        this.usage = usage;
-    }
-
-    @Override
-    public String toString() {
-        return "CountryLanguage{" +
-                "code='" + code + '\'' +
-                ", language='" + language + '\'' +
-                ", official=" + official +
-                ", usage=" + usage +
-                '}';
-    }
+public record CountryLanguage(
+        String code,
+        String language,
+        Boolean official,
+        BigDecimal usage
+) {
 }
