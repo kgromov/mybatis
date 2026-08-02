83784c92bf86c115f036919076e1ad9fca15e713
konstantin <konst.gromov@gmail.com>
2026-08-03 00:18:43 +0300

    Add hint that show difference between defaults in testing

--- src/test/java/org/kgromov/ProdDbMapperTest.java
+++ src/test/java/org/kgromov/ProdDbMapperTest.java
@@ -4,6 +4,10 @@ import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.test.context.ActiveProfiles;
 
+/**
+ * Use main application data source
+ * By default configured to use embedded or test container db
+ */
 @ActiveProfiles("test")
 @MybatisTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
