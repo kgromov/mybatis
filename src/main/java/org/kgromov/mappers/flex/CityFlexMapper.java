a51b7337ed91ba24ac2d6b908ccdb5466f2b6868
konstantin <konst.gromov@gmail.com>
2026-08-12 02:22:09 +0300

    Fix `CityFlexMapper#findAllByNotUniqueByName` - mapping from `Row` aka with `JdbcTemplate/JdbcClient`

--- src/main/java/org/kgromov/mappers/flex/CityFlexMapper.java
+++ src/main/java/org/kgromov/mappers/flex/CityFlexMapper.java
@@ -13,20 +13,23 @@ import java.util.List;
 @Mapper
 public interface CityFlexMapper extends BaseMapper<City> {
 
-  /*  default List<GroupingWithCountView> findAllByNotUniqueByName() {
-        return Db.selectListBySql(
-                "SELECT Name as name, COUNT(*) as count FROM city GROUP BY CountryCode HAVING COUNT(*) > 1  ORDER BY COUNT(*) DESC",
-                GroupingWithCountView.class
-        ).stream()
-                .map(row -> new GroupingWithCountView(row.getString("name"), row.getLong("count")))
+    default List<GroupingWithCountView> findAllByNotUniqueByName() {
+        return Db.selectListBySql("""
+                         SELECT Name, COUNT(*) as count FROM city
+                         GROUP BY Name
+                         HAVING COUNT(*) > 1
+                         ORDER BY COUNT(*) DESC
+                        """
+                ).stream()
+                .map(row -> new GroupingWithCountView(row.getString("NAME"), row.getLong("COUNT")))
                 .toList();
-    }*/
+    }
 
     @Select("""
             SELECT name as grouping, COUNT(*) as count
             FROM city
             GROUP BY name
-            HAVING COUNT(*) > 0
+            HAVING COUNT(*) > 1
             ORDER BY COUNT(*) DESC
             LIMIT #{limit}
             """)
